8d5adebaf64e1cd15ed2a90a3dbc57f082fd0ca7
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-08-15 06:26:17 -0500

    progress

---
+++ src/Common/Util/Math.cs
@@ -0,0 +1,15 @@
+﻿namespace LiveMap.src.Common.Util;
+
+public class Math {
+    public static long AsLong(long x, long z) {
+        return (x & 0xFFFFFFFFL) | (z & 0xFFFFFFFFL) << 32;
+    }
+
+    public static int LongToX(long pos) {
+        return (int)(pos & 0xFFFFFFFFL);
+    }
+
+    public static int LongToZ(long pos) {
+        return (int)(pos >>> 32 & 0xFFFFFFFFL);
+    }
+}
---
+++ src/Server/Renderer/RenderTask.cs
@@ -0,0 +1,40 @@
+﻿namespace LiveMap.Server.Renderer;
+
+public class RenderTask {
+    private readonly RenderQueue renderQueue;
+
+    private bool running = false;
+
+    public RenderQueue RenderQueue {
+        get {
+            return renderQueue;
+        }
+    }
+
+    public RenderTask() {
+        renderQueue = new RenderQueue();
+    }
+
+    public void Start() {
+        running = true;
+    }
+
+    public void Stop() {
+        running = false;
+    }
+
+    public void Run() {
+        if (!running) {
+            return;
+        }
+
+        // todo - process queue
+        //
+    }
+
+    public void Dispose() {
+        Stop();
+
+        renderQueue.Dispose();
+    }
+}
