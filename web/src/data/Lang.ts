14a566f55fe9d57d3a015a82db8305affb769ba2
BillyGalbreath <blake.galbreath@gmail.com>
2024-05-05 07:45:04 -0500

    [ci-skip] codefactor

--- web/src/data/Lang.ts
+++ web/src/data/Lang.ts
@@ -21,28 +21,28 @@ export class Lang {
     private readonly _notifShareFailed: string;
     private readonly _notifCenter: string;
 
-    constructor(lang?: Lang) {
-        this._title = lang?.title ?? 'Vintage Story LiveMap';
-        this._pinned = lang?.pinned ?? 'Pinned';
-        this._unpinned = lang?.unpinned ?? 'Unpinned';
-        this._players = lang?.players ?? 'Players ({cur}/{max})';
-        this._renderers = lang?.renderers ?? 'Map Types';
-        this._copy = lang?.copy ?? 'Copy';
-        this._copyAlt = lang?.copyAlt ?? 'Copy this location to the clipboard';
-        this._paste = lang?.paste ?? 'Paste';
-        this._pasteAlt = lang?.pasteAlt ?? 'Go to a point from the clipboard';
-        this._share = lang?.share ?? 'Share';
-        this._shareAlt = lang?.shareAlt ?? 'Copy a sharable url to the clipboard';
-        this._center = lang?.center ?? 'Share';
-        this._centerAlt = lang?.centerAlt ?? 'Center the map on this point';
-        this._notifCopy = lang?.notifCopy ?? 'Copied location to clipboard';
-        this._notifCopyFailed = lang?.notifCopyFailed ?? 'Could not copy location';
-        this._notifPaste = lang?.notifPaste ?? 'Centered on location from clipboard';
-        this._notifPasteFailed = lang?.notifPasteFailed ?? 'Could not paste location';
-        this._notifPasteInvalid = lang?.notifPasteInvalid ?? 'Not a valid location';
-        this._notifShare = lang?.notifShare ?? 'Copied shareable url to clipboard';
-        this._notifShareFailed = lang?.notifShareFailed ?? 'Could not copy shareable url';
-        this._notifCenter = lang?.notifCenter ?? 'Centered on location';
+    constructor(lang: Lang) {
+        this._title = lang.title ?? 'Vintage Story LiveMap';
+        this._pinned = lang.pinned ?? 'Pinned';
+        this._unpinned = lang.unpinned ?? 'Unpinned';
+        this._players = lang.players ?? 'Players ({cur}/{max})';
+        this._renderers = lang.renderers ?? 'Map Types';
+        this._copy = lang.copy ?? 'Copy';
+        this._copyAlt = lang.copyAlt ?? 'Copy this location to the clipboard';
+        this._paste = lang.paste ?? 'Paste';
+        this._pasteAlt = lang.pasteAlt ?? 'Go to a point from the clipboard';
+        this._share = lang.share ?? 'Share';
+        this._shareAlt = lang.shareAlt ?? 'Copy a sharable url to the clipboard';
+        this._center = lang.center ?? 'Share';
+        this._centerAlt = lang.centerAlt ?? 'Center the map on this point';
+        this._notifCopy = lang.notifCopy ?? 'Copied location to clipboard';
+        this._notifCopyFailed = lang.notifCopyFailed ?? 'Could not copy location';
+        this._notifPaste = lang.notifPaste ?? 'Centered on location from clipboard';
+        this._notifPasteFailed = lang.notifPasteFailed ?? 'Could not paste location';
+        this._notifPasteInvalid = lang.notifPasteInvalid ?? 'Not a valid location';
+        this._notifShare = lang.notifShare ?? 'Copied shareable url to clipboard';
+        this._notifShareFailed = lang.notifShareFailed ?? 'Could not copy shareable url';
+        this._notifCenter = lang.notifCenter ?? 'Centered on location';
     }
 
     get title(): string {
@@ -67,65 +67,65 @@ export class Lang {
 
     get copy(): string {
         return this._copy;
-    };
+    }
 
     get copyAlt(): string {
         return this._copyAlt;
-    };
+    }
 
     get paste(): string {
         return this._paste;
-    };
+    }
 
     get pasteAlt(): string {
         return this._pasteAlt;
-    };
+    }
 
     get share(): string {
         return this._share;
-    };
+    }
 
     get shareAlt(): string {
         return this._shareAlt;
-    };
+    }
 
     get center(): string {
         return this._center;
-    };
+    }
 
     get centerAlt(): string {
         return this._centerAlt;
-    };
+    }
 
     get notifCopy(): string {
         return this._notifCopy;
-    };
+    }
 
     get notifCopyFailed(): string {
         return this._notifCopyFailed;
-    };
+    }
 
     get notifPaste(): string {
         return this._notifPaste;
-    };
+    }
 
     get notifPasteFailed(): string {
         return this._notifPasteFailed;
-    };
+    }
 
     get notifPasteInvalid(): string {
         return this._notifPasteInvalid;
-    };
+    }
 
     get notifShare(): string {
         return this._notifShare;
-    };
+    }
 
     get notifShareFailed(): string {
         return this._notifShareFailed;
-    };
+    }
 
     get notifCenter(): string {
         return this._notifCenter;
-    };
+    }
 }
